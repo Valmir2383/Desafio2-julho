4a7a5d0203d22f6f7bab3c2438c7015b1b022ea1
Valmir Souza <juniorformacao83@gmail.com>
2026-07-21 12:09:33 -0300

    mudando texto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -179,7 +179,7 @@ function AppContent() {
         />
       ) : (
         <div className="empty-state">
-          <h3>Nenhuma tarefa por aqui 📝</h3>
+          <h3>Nenhuma tarefa nesse momento 📝</h3>
           <p>Adicione sua primeira tarefa usando o formulário acima!</p>
         </div>
       )}
